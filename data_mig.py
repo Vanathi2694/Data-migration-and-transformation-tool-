b1f9ab0cd215044d1dd5b1e0b20163dbafeb40a1
vanathi <123821462+Vanathi2694@users.noreply.github.com>
2023-08-15 18:43:37 +0530

    Update data_mig.py

--- data_mig.py
+++ data_mig.py
@@ -1,18 +1,15 @@
-pip install pandas
 pip install boto3
-pip install sqlalchemy
-pip install pymysql
-
 import os
 import zipfile
 import requests
 
-if not os.path.exists("downloaded_files"):
-    os.makedirs("downloaded_files")
+if not os.path.exists("extracted_files"):
+    os.makedirs("extracted_files")
+
 
 url = "https://www.sec.gov/Archives/edgar/daily-index/bulkdata/submissions.zip"
 
-zip_file_path = os.path.join("downloaded_files", "submissions.zip")
+zip_file_path = os.path.join("extracted_files", "submissions.zip")
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
@@ -25,58 +22,87 @@ if response.status_code == 200:
     with open(zip_file_path, "wb") as f:
         f.write(response.content)
     print("Zip file downloaded successfully.")
-
-
-    with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
-        zip_ref.extractall("downloaded_files")
-
-    print("Zip file contents extracted.")
 else:
     print("Failed to download the zip file.")
+
+extracted_files = []
+#target_filenames = ["CIK0000000020.json"]
+target_filenames = ["CIK0000000020.json", "CIK0000001761.json", "CIK0000001750.json", "CIK0000001800.json"]
+with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
+    for target_filename in target_filenames:
+        if target_filename in zip_ref.namelist():
+            extracted_file_path = os.path.join("extracted_files", target_filename)
+            with open(extracted_file_path, "wb") as extracted_file:
+                extracted_file.write(zip_ref.read(target_filename))
+            extracted_files.append(extracted_file_path)
+
+print("JSON files extracted:", extracted_files)
+
 import boto3
 import json
-import pandas as pd
-from sqlalchemy import create_engine
-data_list=[]
-s3_bucket_name = "vanathifinal"
-s3_key = "submissions.json"  # The key under which the JSON data will be stored in S3
 
-
-aws_access_key = "AKIAWK7KZTD6JH7WIOF6"
-aws_secret_key = "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"
+dynamodb = boto3.client('dynamodb', region_name='ap-south-1',aws_access_key_id="AKIAWK7KZTD6JH7WIOF6",
+    aws_secret_access_key="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")
 
 s3_client = boto3.client(
     "s3",
-    aws_access_key_id=aws_access_key,
-    aws_secret_access_key=aws_secret_key
+    aws_access_key_id="AKIAWK7KZTD6JH7WIOF6",
+    aws_secret_access_key="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",
+    region_name='ap-south-1'
 )
 
-data_json=json.dumps(data_list).encode('utf-8')
+s3_bucket_name = "vanathifinal"
+for extracted_file in extracted_files:
+    s3_key = os.path.basename(extracted_file)
+    with open(extracted_file, "rb") as f:
+        s3_client.put_object(
+            Bucket=s3_bucket_name,
+            Key=s3_key,
+            Body=f,
+            ContentType="application/json"
+        )
+    print(f"Uploaded {s3_key} to Amazon S3.")
+
+print("Data successfully uploaded to Amazon S3.")
+
+import boto3
+import json
+
+s3_client = boto3.client('s3',region_name='ap-south-1',aws_access_key_id="AKIAWK7KZTD6JH7WIOF6",
+    aws_secret_access_key="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",
+    )
+dynamodb_client = boto3.client('dynamodb',region_name='ap-south-1',aws_access_key_id="AKIAWK7KZTD6JH7WIOF6",
+    aws_secret_access_key="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",
+    )
+
+bucket_name = 'vanathifinal'
+table_name = 'sub_final'
+
+response = s3_client.list_objects(Bucket=bucket_name)
 
 
-s3_client.put_object(
-    Bucket=s3_bucket_name,
-    Key=s3_key,
-    Body=data_json,
-    ContentType="application/json"
-)
+for obj in response.get('Contents', []):
+    object_key = obj['Key']
+    
+    response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
+    json_content = response['Body'].read().decode('utf-8')
+    
+    
+    data = json.loads(json_content)
+    
+    
+    attribute_map = {}
+    for key, value in data.items():
+        attribute_map[key] = {
+            'S': str(value) if isinstance(value, (str, int, float)) else json.dumps(value)
+        }
+    
+    
+    dynamodb_client.put_item(
+        TableName=table_name,
+        Item=attribute_map
+    )
+    
+    print(f"Uploaded data from {object_key} to DynamoDB")
 
-print("Data successfully stored in Amazon S3.")
-
-rds_host = "database-company.cer7d9d8mbjl.ap-south-1.rds.amazonaws.com"
-rds_port = "3306"
-rds_user = "admin"
-rds_password = "vanathi94"
-rds_db_name = "database-company"
-
-
-engine = create_engine(
-    f"mysql+pymysql://{rds_user}:{rds_password}@{rds_host}:{rds_port}/{rds_db_name}"
-)
-
-df = pd.DataFrame(data_list)
-df.to_sql("data_table", engine, if_exists="replace", index=False)
-
-
-
-print("Data migration and transformation completed successfully.")
+print("Success")
